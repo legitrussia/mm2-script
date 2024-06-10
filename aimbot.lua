276137736a4754c649b134e948f0095e8e585214
legitrussia <153454608+legitrussia@users.noreply.github.com>
2024-06-09 21:02:45 -0300

    aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -4,12 +4,14 @@ local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local LocalPlayer = Players.LocalPlayer
-local Holding = false
 
 _G.AimbotEnabled = true
 _G.TeamCheck = false -- Se definido como true, o script só travará sua mira nos membros da equipe inimiga.
 _G.AimPart = "Head" -- Onde o script de aimbot vai travar.
 _G.Sensitivity = 0 -- Quantos segundos leva para o script de aimbot travar oficialmente no alvo.
+_G.CircleRadius = 100 -- Valor padrão para o raio do círculo de FOV
+
+local Holding = false
 
 local function GetClosestPlayer()
     local MaximumDistance = _G.CircleRadius
@@ -21,36 +23,14 @@ local function GetClosestPlayer()
     end)()
 
     for _, v in next, Players:GetPlayers() do
-        if v.Name ~= LocalPlayer.Name then
-            if _G.TeamCheck == true then
-                if v.Team ~= LocalPlayer.Team then
-                    if v.Character ~= nil then
-                        if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
-                            if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
-                                local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
-                                local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
+        if v ~= LocalPlayer and (not _G.TeamCheck or v.Team ~= LocalPlayer.Team) then
+            if v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health > 0 then
+                local ScreenPoint = Camera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
+                local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
                                 
-                                if VectorDistance < MaximumDistance then
-                                    Target = v
-                                    MaximumDistance = VectorDistance
-                                end
-                            end
-                        end
-                    end
-                end
-            else
-                if v.Character ~= nil then
-                    if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
-                        if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
-                            local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
-                            local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
-                            
-                            if VectorDistance < MaximumDistance then
-                                Target = v
-                                MaximumDistance = VectorDistance
-                            end
-                        end
-                    end
+                if VectorDistance < MaximumDistance then
+                    Target = v
+                    MaximumDistance = VectorDistance
                 end
             end
         end
@@ -76,7 +56,7 @@ UserInputService.InputEnded:Connect(function(Input)
 end)
 
 RunService.RenderStepped:Connect(function()
-    if Holding == true and _G.AimbotEnabled == true then
+    if Holding and _G.AimbotEnabled then
         local closestPlayer = GetClosestPlayer()
         if closestPlayer then
             local aimPart = closestPlayer.Character[_G.AimPart]
