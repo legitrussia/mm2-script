f4943bef267e21067b7f8f5d11fd98f69b23dffd
legitrussia <153454608+legitrussia@users.noreply.github.com>
2024-06-13 19:44:40 -0300

    Script.lua

--- Script.lua
+++ Script.lua
@@ -27,6 +27,7 @@ _G.Sensitivity = 0 -- Quantos segundos leva para o script de aimbot travar ofici
 _G.FOV = 50 -- Campo de Visão (em graus)
 _G.CircleRadius = 100 -- Raio do círculo de FOV
 _G.FOVEnabled = false
+_G.NameTagEnabled = false
 
 local FOV_CIRCLE = Drawing.new("Circle")
 FOV_CIRCLE.Color = Color3.new(1, 1, 1)
@@ -164,6 +165,127 @@ local function espplr(p)
     )
 end
 
+_G.FriendColor = Color3.fromRGB(0, 0, 255)
+_G.EnemyColor = Color3.fromRGB(255, 0, 0)
+_G.UseTeamColor = true
+
+local Holder = Instance.new("Folder", game.CoreGui)
+Holder.Name = "ESP"
+
+local Box = Instance.new("BoxHandleAdornment")
+Box.Name = "nilBox"
+Box.Size = Vector3.new(1, 2, 1)
+Box.Color3 = Color3.new(100 / 255, 100 / 255, 100 / 255)
+Box.Transparency = 0.7
+Box.ZIndex = 0
+Box.AlwaysOnTop = false
+Box.Visible = false
+
+local NameTag = Instance.new("BillboardGui")
+NameTag.Name = "nilNameTag"
+NameTag.Enabled = false
+NameTag.Size = UDim2.new(0, 200, 0, 50)
+NameTag.AlwaysOnTop = true
+NameTag.StudsOffset = Vector3.new(0, 1.8, 0)
+local Tag = Instance.new("TextLabel", NameTag)
+Tag.Name = "Tag"
+Tag.BackgroundTransparency = 1
+Tag.Position = UDim2.new(0, -50, 0, 0)
+Tag.Size = UDim2.new(0, 300, 0, 20)
+Tag.TextSize = 15
+Tag.TextColor3 = Color3.new(100 / 255, 100 / 255, 100 / 255)
+Tag.TextStrokeColor3 = Color3.new(0 / 255, 0 / 255, 0 / 255)
+Tag.TextStrokeTransparency = 0.4
+Tag.Text = "nil"
+Tag.Font = Enum.Font.SourceSansBold
+Tag.TextScaled = false
+
+local LoadCharacter = function(v)
+    repeat wait() until v.Character ~= nil
+    v.Character:WaitForChild("Humanoid")
+    local vHolder = Holder:FindFirstChild(v.Name)
+    vHolder:ClearAllChildren()
+    local b = Box:Clone()
+    b.Name = v.Name .. "Box"
+    b.Adornee = v.Character
+    b.Parent = vHolder
+    local t = NameTag:Clone()
+    t.Name = v.Name .. "NameTag"
+    t.Enabled = true
+    t.Parent = vHolder
+    t.Adornee = v.Character:WaitForChild("Head", 5)
+    if not t.Adornee then
+        return UnloadCharacter(v)
+    end
+    t.Tag.Text = v.Name
+    b.Color3 = Color3.new(v.TeamColor.r, v.TeamColor.g, v.TeamColor.b)
+    t.Tag.TextColor3 = Color3.new(v.TeamColor.r, v.TeamColor.g, v.TeamColor.b)
+    local Update
+    local UpdateNameTag = function()
+        if not pcall(function()
+            v.Character.Humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+            local maxh = math.floor(v.Character.Humanoid.MaxHealth)
+            local h = math.floor(v.Character.Humanoid.Health)
+        end) then
+            Update:Disconnect()
+        end
+    end
+    UpdateNameTag()
+    Update = v.Character.Humanoid.Changed:Connect(UpdateNameTag)
+end
+
+local UnloadCharacter = function(v)
+    local vHolder = Holder:FindFirstChild(v.Name)
+    if vHolder and (vHolder:FindFirstChild(v.Name .. "Box") ~= nil or vHolder:FindFirstChild(v.Name .. "NameTag") ~= nil) then
+        vHolder:ClearAllChildren()
+    end
+end
+
+local LoadPlayer = function(v)
+    local vHolder = Instance.new("Folder", Holder)
+    vHolder.Name = v.Name
+    v.CharacterAdded:Connect(function()
+        pcall(LoadCharacter, v)
+    end)
+    v.CharacterRemoving:Connect(function()
+        pcall(UnloadCharacter, v)
+    end)
+    v.Changed:Connect(function(prop)
+        if prop == "TeamColor" then
+            UnloadCharacter(v)
+            wait()
+            LoadCharacter(v)
+        end
+    end)
+    LoadCharacter(v)
+end
+
+local UnloadPlayer = function(v)
+    UnloadCharacter(v)
+    local vHolder = Holder:FindFirstChild(v.Name)
+    if vHolder then
+        vHolder:Destroy()
+    end
+end
+
+for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+    spawn(function() pcall(LoadPlayer, v) end)
+end
+
+game:GetService("Players").PlayerAdded:Connect(function(v)
+    pcall(LoadPlayer, v)
+end)
+
+game:GetService("Players").PlayerRemoving:Connect(function(v)
+    pcall(UnloadPlayer, v)
+end)
+
+game:GetService("Players").LocalPlayer.NameDisplayDistance = 0
+
+if _G.Reantheajfdfjdgs then
+    return
+end
+
 -- // Aimbot Sector \\ --
 local aimbotSector = aimbotSection.new_sector('Aimbot', 'Left')
 
@@ -171,10 +293,15 @@ aimbotSector.element('Toggle', 'Aimbot', false, function(state)
     ToggleAimbot(state.Toggle)
 end)
 
-aimbotSector.element('Toggle', 'Fov View', false, function(state)
-    toggleFOVCircle(state.Toggle)
+aimbotSector.element('Toggle', 'Team Check', false, function(state)
+    _G.TeamCheck = state.Toggle
+    print("Team Check ativado:", state.Toggle)
+end)
+
+aimbotSector.element('Toggle', 'FOV', false, function(state)
     _G.FOVEnabled = state.Toggle
-    print("FOV View ativado:", state.Toggle)
+    toggleFOVCircle(state.Toggle)
+    print("FOV ativado:", state.Toggle)
 end)
 
 aimbotSector.element('Slider', 'Smoothness', {default = {min = 1, max = 10, default = 1}}, function(value)
@@ -235,6 +362,24 @@ espSector.element('Dropdown', 'ESP Options', {options = {"Box 2D", "Box 3D", "Gl
         end
         print("ESP ativado:", ESP_TYPE)
     end
+end)
+
+espSector.element('Toggle', 'Name Tag', false, function(state)
+    _G.NameTagEnabled = state.Toggle
+    if _G.NameTagEnabled then
+        for _, v in pairs(Players:GetPlayers()) do
+            LoadPlayer(v)
+        end
+        Players.PlayerAdded:Connect(function(v)
+            LoadPlayer(v)
+        end)
+        Players.PlayerRemoving:Connect(function(v)
+            UnloadPlayer(v)
+        end)
+    else
+        Holder:ClearAllChildren()
+    end
+    print("Name Tag ativado:", state.Toggle)
 end)
 
 RunService.RenderStepped:Connect(function()
